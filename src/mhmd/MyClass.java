382743a8bd48245e5fc617c6bb087e95d9966f31
jurgen <jurgen@qasedo>
2020-02-08 13:07:44 +0200

    this is the third version

--- src/mhmd/MyClass.java
+++ src/mhmd/MyClass.java
@@ -1,9 +1,11 @@
 package mhmd;
 
 public class MyClass {
-
+	public static void print() {
+		System.out.print("Hellooo World !!!");
+	}
 	public static void main(String[] args) {
-		System.out.print("Hellooo hmoooooooda");
+		print();
 	}
 
 }
